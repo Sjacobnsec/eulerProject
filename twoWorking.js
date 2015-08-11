eb8d9e0d7173221840942fde6de48e4ac49d5380
Sam <s.jacobnsec@gmail.com>
2015-08-10 21:06:50 -0500

    Update twoWorking.js
    This program is incomplete, it does not terminate properly.

--- twoWorking.js
+++ twoWorking.js
@@ -1,7 +1,11 @@
 var myArray = [1, 2];
 
+/* The for loop creates the i variable and the fib sequence relies on it
+this creates problems when I change the termination variable to anything other than 
+a 10 */
+
 for (i = 0; i < 10; i++){
-    myArray.push((myArray[i + 1]) + (myArray[i]));
+    myArray.push((myArray[i + 1]) + (myArray[i])); //fib sequence 
 	if (myArray[i] % 2 !== 0) {
 		console.log("Odd");
 		delete myArray[i];
