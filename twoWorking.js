60b0f925284b0817189258fb6acd9999065427de
Nsec <S.jacobnsec@gmail.com>
2015-08-10 19:51:10 -0500

    odd number remover works and is inside the first for loop now

--- twoWorking.js
+++ twoWorking.js
@@ -1,13 +1,13 @@
 var myArray = [1, 2];
 
 for (i = 0; i < 10; i++){
-    myArray.push((myArray[i + 1]) + (myArray[i])); 
-}
-console.log(myArray);
-
-for (i = 0; i < 10; i++){
+    myArray.push((myArray[i + 1]) + (myArray[i]));
 	if (myArray[i] % 2 !== 0) {
 		console.log("Odd");
 		delete myArray[i];
-	}
+	} 
 }
+
+
+console.log(myArray);
+
