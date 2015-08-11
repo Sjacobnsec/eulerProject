a10ce14aa1b543c8ed8bb9a4f3c6e1aaec33bcdb
Sam <s.jacobnsec@gmail.com>
2015-08-11 18:38:40 -0500

    Divisibility rules
    if a number is divisible by 2 without a remainder than it is not a prime number, can start there possibly.

--- threeWorking.js
+++ threeWorking.js
@@ -5,10 +5,14 @@ var triTop = 600851475143;
 var myArray = [];
 
 for (i = 0; i < 72; i++) {
-    var consult = (triTop / i);
+
     //console.log(consult);
-    if (consult % consult === 0) {
-        myArray.push(consult); 
+    if (i % 2 === 0) {
+        //that number is not a prime 
+        console.log(i + " " + "That number is not a prime");
+    } else {
+        var consult = (triTop / i);
+        myArray.push(consult);
     }
 }
 
