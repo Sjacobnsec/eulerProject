22bcdafb5adeb5ceaa34e9ed7bc8cb45263a522a
Sam <s.jacobnsec@gmail.com>
2015-08-11 18:22:10 -0500

    Working it out through code

--- threeWorking.js
+++ threeWorking.js
@@ -1,3 +1,16 @@
 // find the largest prime factor of  
 // 600851475143
 
+var triTop = 600851475143;
+var myArray = [];
+
+for (i = 0; i < 72; i++) {
+    var consult = (triTop / i);
+    //console.log(consult);
+    if (consult % consult === 0) {
+        myArray.push(consult); 
+    }
+}
+
+//console.log(consult);
+console.log(myArray);
